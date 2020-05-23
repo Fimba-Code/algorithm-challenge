21c38659f7f03d52e3bbdfc6aa3501a8f39fa87b
Sténio Jacinto <stexor12@gmail.com>
2020-05-23 14:53:55 +0100

    Add python solutions (#3)
    * Add python solutions
    
    * Refactor logic for negative numbers

---
+++ solutions/python/challenge_1.py
@@ -0,0 +1,29 @@
+# 1. Receber input do usuário
+# input é recebido como string
+num = input("input: ")
+
+# 2. Verificar se o input é um número inteiro
+try:
+    # Tentar converter o input para inteiro
+    int(num)
+except ValueError:
+    # Em caso de erro sair do programa
+    print("Introduza um número inteiro")
+    exit(0)
+
+
+#3. Obter o valor absoluto do número (em caso de negativos)
+num = str(abs(int(num)))
+
+
+# 4. Percorrer cada 'digito' do número e acumular a soma 
+# dentro da var 'result'
+result = 0
+
+for digit in num:
+    # cada digito é primeiro convertido para int e 
+    #só depois é acrescentado à soma
+    result += int(digit)
+ 
+# 5. Mostrar resultado ao usuário    
+print(f"output: {result}")
